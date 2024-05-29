4649155c49459f0da4fd510f024504e19ea5c148
MichaelSpiridonov <michael.sp579@gmail.com>
2024-05-29 17:34:29 +0300

    Add maxDiameter as ball size limiter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict'
 var gSize = 150
 
-function onBallClick(elBall) {
-    if (gSize > 400) gSize = 100
+function onBallClick(elBall, maxDiameter) {
+    if (gSize > maxDiameter) gSize = 100
     elBall.style.width = `${gSize}px`
     elBall.style.height = `${gSize}px`
     elBall.innerText = gSize
