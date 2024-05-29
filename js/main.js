e6275717622a19072ea51a475136cbb09b273143
MichaelSpiridonov <michael.sp579@gmail.com>
2024-05-29 17:52:27 +0300

    Add 3rd Ball & logic of swapping between ball1 & 2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,39 @@
 var gSize = 150
 
 function onBallClick(elBall, maxDiameter) {
-    if (gSize > maxDiameter) gSize = 100
+    if (gSize > maxDiameter || gSize > 400) gSize = 100
     elBall.style.width = `${gSize}px`
     elBall.style.height = `${gSize}px`
     elBall.innerText = gSize
     elBall.style.backgroundColor = getRandomColor()
     gSize += getRandomInt(20, 60)
+}
+
+function swapColorAndSize() {
+
+    //Define Ball1 and Ball2
+    var ball1 = document.querySelector('.ball1')
+    var ball2 = document.querySelector('.ball2')
+
+    //Obtain Size and colors
+    var ball1Color = ball1.style.backgroundColor
+    var ball2Color = ball2.style.backgroundColor
+    var ball1Size = ball1.style.height
+    var ball2Size = ball2.style.height
+
+
+    //Set Color
+    ball1.style.backgroundColor = ball2Color
+    ball2.style.backgroundColor = ball1Color
+
+    //Set Size
+    ball1.style.width = ball2Size
+    ball1.style.height = ball2Size
+    ball2.style.width = ball1Size
+    ball2.style.height = ball1Size
+
+    //Set InnerText To match
+    ball1.innerText = ball2Size.split('px')[0]
+    ball2.innerText = ball1Size.split('px')[0]
+
 }
